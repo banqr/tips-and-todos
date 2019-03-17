7478436da7fc404c1a0c60f1cf09149683b75ee5
Radovan Draskovic <radovandraskovic@gmail.com>
2019-03-18 00:19:07 +0100

    Dugme za brisanje tipsova radi. Dodao onclick event koji poziva delete rutu na tipsu

--- client/get_tips.js
+++ client/get_tips.js
@@ -1,9 +1,14 @@
 const API_URL = 'http://localhost:5000/tips'
+const API_URL_DEL = 'http://localhost:5000/tips/'
 const tips_div = document.querySelector('#tips')
 import { callAPI } from './utils.js'
 
+const check = (ajdi) => {
+        console.log(ajdi)  
+}
+
 const kreiraj_tips = (data) => {
-    data.forEach(el => {
+    data.forEach((el, i) => {
         const li = document.createElement('li')
 
         const naslov = document.createElement('h5')
@@ -16,22 +21,31 @@ const kreiraj_tips = (data) => {
         opis.textContent = el.opis
         
         const dugme = document.createElement('button')
+        const id = data[i]._id
         dugme.className = 'button-primary'
-        dugme.id = 'delete_tip'
+        dugme.id = id
         dugme.textContent = 'Delete'
 
+        //ovako se dodaje event kad dinamički kreiraš DOM element!!!!
+        dugme.onclick = (() => {fetch(API_URL_DEL + dugme.id, {
+            method: 'DELETE'
+        })}) 
+        
         li.appendChild(naslov)
         li.appendChild(jezik)
         li.appendChild(opis)
         li.appendChild(dugme)
 
-        tips_div.appendChild(li)
-        
+        tips_div.appendChild(li)   
     })
 }
+
+
 
 callAPI(API_URL)
     .then(result => kreiraj_tips(result))
     .catch(err => {
         console.log(`Greška je ${err}`)
     })
+
+
